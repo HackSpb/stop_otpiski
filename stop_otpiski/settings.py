217f6e00010a66ced09f777c89f11d1cf6ec22bb
etimvr <erntimur@gmail.com>
2019-12-14 22:47:11 +0300

    add postgis to settings

--- stop_otpiski/settings.py
+++ stop_otpiski/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'stop_otpiski.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'db_name',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
